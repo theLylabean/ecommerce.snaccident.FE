cf7890f10109aa0993507109d1ee39ce71152429
Lyla Lynn <lyladlynn@gmail.com>
2025-06-24 18:26:00 -0500

    passing props in to component in app.jsx and added useStates

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 
 function App() {
   const [token, setToken] = useState(() => localStorage.getItem("authToken"));//pending Yonas "authToken" local storage
+  const [products, setProducts] = useState([]);
+  const [singleProduct, setSingleProduct] = useState([]);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("authToken");//pending Yonas "authToken" local storage
@@ -20,6 +22,24 @@ function App() {
         <Route path="/orders" element={token ? <OrderList token={token} /> : <Navigate to="/login" replace />}
         />
         <Route path="/orders/:id" element={token ? <OrderDetails token={token} /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path='/products'
+          element={
+            <Products
+            products={products}
+            setProducts={setProducts}
+            />
+          }
+        />
+        <Route
+          path='/products/:id'
+          element={
+            <SingleProduct
+            singleProduct={singleProduct}
+            setSingleProduct={setSingleProduct}
+            />
+          }
         />
       </Routes>
     </Router>
---
+++ src/components/Products.jsx
@@ -0,0 +1,6 @@
+
+const Products = () => {
+    const handleClick = () => {
+        
+    }
+}
