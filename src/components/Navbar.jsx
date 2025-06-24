fe6cfb772f339a8b576d6b7164fc74618417c303
Claudia <cludama83@gmail.com>
2025-06-24 19:40:16 -0400

    Navbar.jsx completed, added import and route to app.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = ({ token, setToken }) => { 
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("authToken");
+        setToken(null);
+        navigate("/login");
+    }
+
+return (
+    <nav>
+        <h1>Name of Shop</h1>
+        <ul>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/account">Account</Link></li>
+            {token ? (
+                <li><button onClick={ handleLogout }>Logout</button></li>
+            ) : ( 
+                <li><Link to="/login">Login</Link></li>
+            )}
+        </ul>
+    </nav>
+)
+};
+export default Navbar;
