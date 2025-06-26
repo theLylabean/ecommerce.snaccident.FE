c503e07b72e0179020f5383a75cea4f9b426ef6f
Lyla Lynn <lyladlynn@gmail.com>
2025-06-25 23:24:55 -0500

    updating account page

--- src/api/index.js
+++ src/api/index.js
@@ -17,8 +17,20 @@ const getSingleProduct = async (id) => {
         const result = await res.json();
         return result;
     } catch (error) {
-        console.error({ error: 'Error getting book by id.'})
+        console.error({ error: 'Error getting product by id.'})
     }
 }
 
-export { getProducts, getSingleProduct }
+const getUserReviews = async (token) => {
+    try {
+        const res = await fetch(`${baseUrl}/users/reviews`, {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+        const result = await res.json();
+        return result;
+    } catch (error) {
+        console.error({ error: 'Error getting review by user id.'})
+    }
+}
+
+export { getProducts, getSingleProduct, getUserReviews }
